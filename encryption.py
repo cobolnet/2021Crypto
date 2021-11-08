b00477737e0f638f20582b999cdac2fe8834625d
Floodnut <gsniper777@gmail.com>
2021-11-08 17:57:30 +0900

    1차 수정

--- encryption.py
+++ encryption.py
@@ -1,16 +1,3 @@
 import hexasolver as hexa
 
 
-'''
-1. Box.fill_box(key) -> 인자 key 256 bit == 64 byte 0~f까지만 들어가는 키
-    키 '0123412451f12351212....'
-
-
-2. Key 전달 형태 다시 전하기
-
-
-3. 코드 병합 Main 16라운드 -> 암호화 루틴 완성하기
-
-
-
-'''
