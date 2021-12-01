83d5a8dd41e9fddbb655feae3ea742e28f3d2558
nsih6571 <57556313+nsih6571@users.noreply.github.com>
2021-12-02 01:59:29 +0900

    Revert "Merge branch 'main' of https://github.com/students16/2021crypto"
    This reverts commit 4a2cb97a6702512621bb56e533a691364eeadff7, reversing
    changes made to dc064af625b3f689d9b47d19864c93ef40d3dbca.

--- padding.py
+++ padding.py
@@ -2,12 +2,18 @@
 #4조
 #20163081 정금종
 
-def zeroPadding(plain):
-    k = plain.encode('utf-8')
-    bsize = len(k) 
-    mod = bsize % 32 
+def pkcs7(block):
+    k = block.encode('utf-8')
+    bsize = len(k)
 
-    if (mod != 0): 
-        k += bytes(32 - mod)
-     
-    return k
+    if (bsize <= 64):
+        paddingSize = 64 - bsize
+        t = bytes.fromhex(str(hex(paddingSize)[2:4]))
+
+        k += (t * paddingSize)
+    return str(k.decode('utf-8'))
+
+#tt = pkcs7("테스트")
+
+#print(tt)
+#print(tt.encode('utf-8'))
